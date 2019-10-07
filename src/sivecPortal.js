8cdb4d7c8cec999f90a746ec17fe5274ba2402e0
leo-bianchi <leo.c.bianchi@hotmail.com>
2019-10-07 09:31:32 -0300

    Creates sivecPortal.js

---
+++ src/sivecPortal.js
@@ -0,0 +1,73 @@
+/*jshint esversion: 8 */
+const select = require('puppeteer-select');
+const fromEntries = require('object.fromentries');
+const removeAccents = require('./libs/removeAccents.js');
+const chromeInstance = require('./libs/getChrome.js');
+
+const sivec = 'http://ec2-18-231-116-58.sa-east-1.compute.amazonaws.com/sivec/pagina3-pesquisa-rg.html';
+
+module.exports = async function sivecPortal() {
+
+  try {
+
+    const page = await chromeInstance(sivec);
+
+    await page.waitForSelector('#idValorPesq');
+    // isto tem que ser alterado para o input do usuário no form.
+    await page.$eval('#idValorPesq', el => el.value = '1.157.644');
+
+    await page.click('#procurar');
+
+    await page.waitForNavigation({
+      waitUntil: 'load',
+    });
+
+    // trocar por input do usuário
+    const a = await select(page).getElement('a:contains(1.157.644)');
+
+    if (a) {
+      await a.click();
+    } else {
+      throw new Error("Link not found");
+    }
+
+    await page.waitForNavigation({
+      waitUntil: 'load',
+    });
+
+    const data = await page.$$eval('table tr td span',
+      spans => spans.map((span) => {
+        return span.innerText.trim().replace(':', '').replace('º', '');
+      }));
+
+    Array.prototype.toObject = await
+
+    function() {
+      let r = {};
+
+      for (let i = 0; i < this.length; i += 2) {
+        let key = removeAccents(this[i]),
+          value = this[i + 1];
+        r[key] = value;
+      }
+      return r;
+    };
+
+    var obj = await data.toObject();
+
+    // Global
+    obj = fromEntries(
+      Object.entries(obj).map(([k, v]) => [k.toLowerCase(), v.toUpperCase()])
+    );
+
+    await page.close();
+
+    return obj;
+
+  } catch (error) {
+    console.log('Portal Sivec =>', error);
+    if (typeof page !== 'undefined') {
+      page.close();
+    }
+  }
+};
