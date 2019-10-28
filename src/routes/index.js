77cd10678265e8ebbb09efc26b1b1b2642316bf7
leo-bianchi <leo.c.bianchi@hotmail.com>
2019-10-28 16:43:40 -0300

    Bomba patch 200% atualizado em portugues

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 const db_service = require('../service/db_service');
 const parse = require('../libs/parseObject.js');
 const response = require('../model/responseModel');
-const scrapping = require('../service/scrappingService');
+const scrapping = require('../service/scrapping_service');
 
 router.get('/search', async function(req, res) {
   response.status = 200;
