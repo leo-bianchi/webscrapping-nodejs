6372fc645388945cb32aa0df40826f296e54497a
leo-bianchi <leo.c.bianchi@hotmail.com>
2019-10-29 14:48:17 -0300

    Set config.js file to connect with db

--- src/libs/conn.js
+++ src/libs/conn.js
@@ -1,39 +1,33 @@
 const mysql = require('mysql');
+const config = require('../config/config.js');
 
-const con =  mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "white_cheese",
-        port: 3306
-        });
+const con = mysql.createConnection(config);
 
 con.connect(function(err) {
-    if(err){
-        console.log(err);
-        console.log('Error connecting to Db');
-        return;
-      }
+  if (err) {
+    console.log(err);
+    console.log('Error connecting to Db');
+    return;
+  }
   console.log('Database connected');
 });
 
 function select(query, callback) {
-    con.query(query,function(err,rows){
-        if(err) throw err;
-        return callback(rows);
-    });
+  con.query(query, function(err, rows) {
+    if (err) throw err;
+    return callback(rows);
+  });
 }
 
 function insert(query, callback) {
-    con.query(query,function(err,rows){
-        if(err) throw err;
-        return callback(rows);
-    });
+  con.query(query, function(err, rows) {
+    if (err) throw err;
+    return callback(rows);
+  });
 }
 
 
-module.exports =
-{
-    select : select,
-    insert: insert
+module.exports = {
+  select: select,
+  insert: insert
 };
