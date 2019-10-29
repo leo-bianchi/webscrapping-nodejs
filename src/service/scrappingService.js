95ed1c605683ae44fdec54aac5483a348c06b057
leo-bianchi <leo.c.bianchi@hotmail.com>
2019-10-29 18:40:14 -0300

    Temporary feature on scrappingService

--- src/service/scrappingService.js
+++ src/service/scrappingService.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 8, node: true*/
+
 const nodemailer = require('nodemailer');
 const sivecPortal = require('../portals/sivecPortal.js');
 const sielPortal = require('../portals/sielPortal.js');
@@ -10,6 +12,16 @@ const cagedPortal = require('../portals/cagedPortal.js');
 const dbService = require('./dbService.js');
 const response = require('../model/responseModel.js').responseModel;
 
+(async function() {
+  console.log(await getResult());
+})();
+
+async function getResult() {
+  let obj = await detranPortal();
+
+  return obj;
+}
+
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
@@ -18,24 +30,26 @@ const transporter = nodemailer.createTransport({
     user: 'gbmadeira27@gmail.com',
     pass: 'joao2000'
   },
-  tls: { rejectUnauthorized: false }
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 
 async function scrapAll(parm, id) {
 
 
-
-  let [sivec, cadesp, siel, detran] = await Promise.all(
-     [
-          sivecPortal(parm.rg),
-          cadespPortal(),
-          /*sielPortal(),
-          detranPortal(),
-          arpenpPortal(),
-          infocrimPortal(),
-          censecPortal()*/
-     ]);
+  //
+  // let [sivec, cadesp, siel, detran] = await Promise.all(
+  //    [
+  //         sivecPortal(parm.rg),
+  //         cadespPortal(),
+  //         /*sielPortal(),
+  //         detranPortal(),
+  //         arpenpPortal(),
+  //         infocrimPortal(),
+  //         censecPortal()*/
+  //    ]);
 
 
 
@@ -50,10 +64,10 @@ async function scrapAll(parm, id) {
     from: 'gbmadeira27@gmail.com',
     to: response.data[0].EMAIL,
     subject: 'Sua consulta foi concluída',
-    text: 'Sua consulta ao cpf '+parm.cpf+' foi concluida e está disponivel no portal'
+    text: 'Sua consulta ao cpf ' + parm.cpf + ' foi concluida e está disponivel no portal'
   };
 
-  transporter.sendMail(mailOptions, function(error, info){
+  transporter.sendMail(mailOptions, function(error, info) {
     if (error) {
       console.log(error);
     } else {
